e340097da20aaeaf3fe0c370b53d7632c3caabd3
Flow4Work <147015641+Flow4Work@users.noreply.github.com>
2026-07-14 21:56:27 +0900

    Add Seoul tourism public data admin page

---
+++ app/admin/public-data/page.tsx
@@ -0,0 +1,7 @@
+import RedTableConnector from "@/components/RedTableConnector";
+
+export const metadata = { title: "서울 음식관광 OPEN API | MapForYou" };
+
+export default function PublicDataAdminPage() {
+  return <RedTableConnector />;
+}
